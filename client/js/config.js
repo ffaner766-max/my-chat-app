f954d01a0a64c1b6b9c7e636b4a7f62e4c8c4ded
ffaner766-max <ffaner766@gmail.com>
2026-06-29 02:20:22 +0700

    Add files via upload

---
+++ client/js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://dnhdlimkkwrvjlqpflmd.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuaGRsaW1ra3dydmpscXBmbG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NjIxMjEsImV4cCI6MjA5ODIzODEyMX0.kZ_tjQiSF3fcOViQqghnWRa7jfxuefs__ELHQXBMZV8',
+    CASE_COOLDOWN: 1500,
+    AVATAR_COLORS: ['#5865f2', '#faa81a', '#f47b1a', '#23a55a', '#ed4245', '#eb459e', '#f0b232']
+};
+export default CONFIG;
