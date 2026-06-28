b744f58769ad7804bd6f0fd3f53bf752b47d4819
ffaner766-max <ffaner766@gmail.com>
2026-06-29 02:19:20 +0700

    Delete client directory

--- client/js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    SERVER_URL: 'http://localhost:3000',
-    CASE_COOLDOWN: 1500,
-    AVATAR_COLORS: ['#5865f2', '#faa81a', '#f47b1a', '#23a55a', '#ed4245', '#eb459e']
-};
-export default CONFIG;
