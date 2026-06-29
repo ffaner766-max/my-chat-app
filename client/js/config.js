c4eff056b1b51a83808f2a8024296a03e8bc8e9e
ffaner766-max <ffaner766@gmail.com>
2026-06-30 02:51:53 +0700

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,10 +1,6 @@
 const CONFIG = {
-    // 👇 ТВОЙ URL
     SUPABASE_URL: 'https://dnhdlimkkwrvjlqpflmd.supabase.co',
-    
-    // 👇 ТВОЙ ANON PUBLIC КЛЮЧ (ПЕРВЫЙ ИЗ ДВУХ)
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuaGRsaW1ra3dydmpscXBmbG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NjIxMjEsImV4cCI6MjA5ODIzODEyMX0.kZ_tjQiSF3fcOViQqghnWRa7jfxuefs__ELHQXBMZV8',
-    
     CASE_COOLDOWN: 1500,
     AVATAR_COLORS: ['#5865f2', '#faa81a', '#f47b1a', '#23a55a', '#ed4245', '#eb459e', '#f0b232']
 };
