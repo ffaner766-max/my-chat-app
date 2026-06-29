d97f8acd587f710ab1f3c79d2d3167a24ff2e6e8
ffaner766-max <ffaner766@gmail.com>
2026-06-30 03:25:12 +0700

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    SUPABASE_URL: 'https://dnhdlimkkwrvjlqpflmd.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuaGRsaW1ra3dydmpscXBmbG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NjIxMjEsImV4cCI6MjA5ODIzODEyMX0.kZ_tjQiSF3fcOViQqghnWRa7jfxuefs__ELHQXBMZV8',
+    SUPABASE_URL: 'https://nrysusyrumpobpirkagl.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yeXN1c3lydW1wb2JwaXJrYWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NTcyMTMsImV4cCI6MjA5ODMzMzIxM30.I0EHcljV8wShmFjW8pvQKR8JDGkPfbz5puB3yVmXUyE',
     CASE_COOLDOWN: 1500,
     AVATAR_COLORS: ['#5865f2', '#faa81a', '#f47b1a', '#23a55a', '#ed4245', '#eb459e', '#f0b232']
 };
